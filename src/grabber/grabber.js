3bd4b441ae5720252df59b1f8f4d88bca37524bc
Luchanskiy Oleg <luchansodev@gmail.com>
2018-05-12 01:21:20 +0300

    feat(*): some fix

--- src/grabber/grabber.js
+++ src/grabber/grabber.js
@@ -1,9 +1,12 @@
 const puppeteer = require("puppeteer");
+const dateformat = require('dateformat');
 const fs = require('fs');
 const URL = require('URL');
 const selectors = require("./selectors");
 
 const BASE_URL = "https://pass.rzd.ru/";
+const ONE_DAY = 1000 * 60 * 60 * 24;
+const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36';
 
 const logger = interceptedRequest => {
   const url = URL.parse(interceptedRequest.url());
@@ -23,7 +26,7 @@ const blocker = async (request) => {
   const url = URL.parse(request.url());
   if (url.host !== 'pass.rzd.ru') {
     console.log('block', url.host);
-    request.abort();
+    request.abort('aborted');
   }
 }
 
@@ -38,23 +41,36 @@ const saveData = (data) => {
     if (rowStr && !err) {
       parsedData = JSON.parse(rowStr);
     }
-    fs.writeFile('src/data.json', JSON.stringify([...parsedData, ...data]), () => {});
+    fs.writeFile('src/data.json', JSON.stringify([...parsedData, ...list]), () => {});
   });
 }
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    devtools: true,
+  });
   const page = await browser.newPage();
+  await page.goto(BASE_URL);
+  // await browser.close();
+  // process.exit(0);
+  // return;
 
   await page.setCacheEnabled(true);
+  await page.setUserAgent(USER_AGENT);
   await page.setDefaultNavigationTimeout(45000);
+  // await page.setRequestInterception(true);
   await page.setViewport({ width: 1600, height: 700 });
 
   page.on("request", logger);
   // page.on("request", blocker);
   page.on("response", responseHook);
 
-  await grab(page, browser, '11.06.2018' 10);
+  for (let i = 0; i < 90; i++) {
+    const date = new Date(Date.now() + ONE_DAY * i);
+    console.log('grab for', date.toDateString());
+    await grab(page, browser, dateformat(date, 'dd.mm.yyyy'), 10);
+    await page.waitFor(5000);
+  }
 
   await browser.close();
 })();
@@ -67,7 +83,10 @@ const grab = async (page, browser, date, retry) => {
     await page.click(selectors.mainFrom);
     await page.click(selectors.mainTo);
     const inputDate = await page.$(selectors.mainDateInput);
-    inputDate.type(date);
+    for (let i = 0; i < 10; i++) {
+      await inputDate.press('Backspace', { delay: 50 });
+    }
+    await inputDate.type(date, { delay: 50 });
 
     await page.screenshot({ path: "screenshots/main-presubmit.png" });
 
