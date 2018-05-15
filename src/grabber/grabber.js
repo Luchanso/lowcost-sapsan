c8a74f02750e17f517cc0bc089071066d65d7a5d
Luchanskiy Oleg <luchansodev@gmail.com>
2018-05-15 11:46:10 +0300

    feat(graber): add new grabber

--- src/grabber/grabber.js
+++ src/grabber/grabber.js
@@ -1,7 +1,7 @@
 let request = require("request-promise");
 const { blue } = require("chalk");
 const dateformat = require("dateformat");
-const fs = require("fs");
+const fs = require("fs-extra");
 const sleep = require("sleep-promise");
 
 var jar = request.jar();
@@ -11,6 +11,27 @@ const BASE_URL = "https://pass.rzd.ru/";
 const ONE_DAY = 1000 * 60 * 60 * 24;
 const USER_AGENT =
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36";
+
+const saveData = async data => {
+  console.log(data.tp);
+  const list = data.tp[0].list;
+  console.log(blue("saving data"));
+
+  let rowStr = "[]";
+  try {
+    rowStr = await fs.readFile("src/data.json");
+  } catch (error) {}
+  const parsedData = JSON.parse(rowStr);
+  return fs.writeFile(
+    "src/data.json",
+    JSON.stringify([...parsedData, ...list])
+  );
+};
+const cleanData = async () => {
+  try {
+    await fs.unlink("src/data.json");
+  } catch (error) {}
+};
 
 const grab = async date => {
   const form = {
@@ -28,18 +49,22 @@ const grab = async date => {
     jar
   };
 
+  console.log(blue("fetching rid"));
   const { RID } = await request.post(options);
   options.form.rid = RID;
-  await sleep(1000);
+  await sleep(1500);
+  console.log(blue("fetching data"));
   const result = await request.post(options);
-
-  console.log(blue(JSON.stringify(result)));
+  await saveData(result);
 };
 
 (async () => {
+  await cleanData();
+
   for (let i = 0; i < 1; i++) {
     const date = new Date(Date.now() + ONE_DAY * i);
     console.log(blue("grab"), "for", date.toDateString());
     await grab(dateformat(date, "dd.mm.yyyy"));
+    await sleep(1000);
   }
 })();
